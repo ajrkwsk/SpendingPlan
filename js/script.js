2dc900cbf75e8d566029a9c6a02c979dc38c33b9
ajrkwsk <angelika_jurkowska@wp.pl>
2021-10-25 21:29:45 +0200

    data update

--- js/script.js
+++ js/script.js
@@ -9,15 +9,27 @@ const category = document.querySelector('#category');
 const title = document.querySelector('#title');
 const amount = document.querySelector('#amount');
 const error = document.querySelector('.error');
-const card = document.querySelector('.card');
+const card = document.querySelectorAll('.card');
 const cost = document.querySelectorAll('.cost');
 const costAmount = document.getElementById('cost-amount');
 const totalCost = document.querySelector('.total-costs');
+const headerCard = document.querySelector('.header-card');
+const cardDocumentation = document.querySelector('.documentation');
+const cardBasicState = document.querySelector('.basic-state');
+const cardInsideFinish = document.querySelector('.inside-finish');
+const cardOutsideFinish = document.querySelector('.outside-finish');
+let costDocumentation = document.querySelector('.cost-documentation');
+const costBasicState = document.querySelector('.cost-basic-state');
+const costInsideFinish = document.querySelector('.cost-inside-finish');
+let costOutsideFinish = document.querySelector('.cost-outside-finish');
 
 let expenseID = 0;
 let bodyCard;
 let costArray = [];
 let newCard;
+let newCost;
+let totalCostArray = [];
+
 
 const changeForm = () => {
     if(category.value == 0 || title.value == '' || amount.value == ''){
@@ -45,6 +57,9 @@ const saveCost = () => {
         bodyCard = document.createElement('div');
         bodyCard.classList.add('body-card');
         bodyCard.setAttribute('id', expenseID);
+        additionAllCost();
+        // additionCost();
+        // additionDocumentionCost();
 
         bodyCard.innerHTML = `
             <div class="body-title">
@@ -53,11 +68,9 @@ const saveCost = () => {
                 <p id="cost-amount">${amount.value} zł</p>
             </div>
             <button onClick=deleteCost(${expenseID})><i class="fas fa-times"></i></button>`;
-
-        // card.appendChild(bodyCard);
         expenseID++;
         changeCategory();
-        additionCost();
+        // additionAllCost();
         cleanForm();
     } 
 }
@@ -65,31 +78,73 @@ const saveCost = () => {
 const changeCategory = () => {
     switch(category.value) {
         case '1':
-            newCard = document.querySelector('.documentation');
-            newCard.appendChild(bodyCard);
+            cardDocumentation.appendChild(bodyCard);
             break;
         case '2':
-            newCard = document.querySelector('.basic-state');
-            newCard.appendChild(bodyCard);
+            cardBasicState.appendChild(bodyCard);
             break;
         case '3':
-            newCard = document.querySelector('.inside-finish');
-            newCard.appendChild(bodyCard);
+            cardInsideFinish.appendChild(bodyCard);
             break;
         case '4':
-            newCard = document.querySelector('.outside-finish');
-            newCard.appendChild(bodyCard);
+            cardOutsideFinish.appendChild(bodyCard);
             break;
     }
 }
 
 const additionCost = () => {
-    let sum = 0;
+    // let sum = 0;
+    
+    // costArray.push(amount.value);
+    // for(i=0; i < costArray.length; i++) {
+    //     sum += parseInt(costArray[i]);
+    // }
+
+    // newCost.textContent = sum;
+
+
+
     costArray.push(amount.value);
     for(i=0; i < costArray.length; i++) {
         sum += parseInt(costArray[i]);
     }
-    cost.textContent = sum;
+
+    // let newCost;
+    costDocumentation.textContent = sum;
+    if(category.value == 2) {
+        costBasicState.textContent = sum;
+    } else if (category.value == 3) {
+        costInsideFinish.textConten = sum;
+    } else {
+        costOutsideFinish.textContent = sum;
+    }
+}
+
+const additionDocumentionCost = () => {
+     let documentationArray = [];
+     let basicStaticArray = [];
+     let sumCost = 0;
+
+     if(category.value == 1) {
+         documentationArray.push(amount.value);
+     } 
+
+    for(i=0; i < documentationArray.length; i++) {
+        sumCost += parseInt(documentationArray[i]);
+    } 
+    costDocumentation.textContent = sumCost;
+
+    console.log(sumCost);
+    console.log(documentationArray);
+}
+
+const additionAllCost = () => {
+    let allSum = 0;
+    totalCostArray.push(amount.value);
+    for(i=0; i < totalCostArray.length; i++) {
+            allSum = allSum + parseInt(totalCostArray[i]);
+    }
+    totalCost.textContent = parseInt(allSum);
 }
 
 const deleteAll = () => {
@@ -98,8 +153,8 @@ const deleteAll = () => {
 
 const deleteCost = id => {
     costToDelete = document.getElementById(id);
-    // card.removeChild(costToDelete);
-    newCard.removeChild(costToDelete);
+    card.removeChild(costToDelete);
+    console.log(card);
 }
 
 addCostBtn.addEventListener('click', function() {
